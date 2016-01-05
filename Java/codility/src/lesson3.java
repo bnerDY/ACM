f3aee1ed6dea50bbaa0b0dc8d1a5b674b01dc70c
martin <dingxin.yu@uqconnect.edu.au>
2016-01-05 21:10:05 +0000

    no message

--- Java/codility/src/lesson3.java
+++ Java/codility/src/lesson3.java
@@ -3,7 +3,35 @@
  */
 public class lesson3 {
 
+    /**
+     * Time Complexity.
+     * Lesson1. Calculate the minimum difference among the list.
+     * input [3,1,2,4,3]
+     * output 1
+     */
+    public static int tapeEquilibrium(int[] A) {
+        int leftSum = A[0];
+        int rightSum = 0;
+        for (int i = 1; i < A.length; i++) {
+            rightSum += A[i];
+        }
+        int minDif = Math.abs(leftSum - rightSum);
+        for (int i = 1; i < A.length - 1; i++) {
+            leftSum += A[i];
+            rightSum -= A[i];
+            minDif = Math.min(minDif, Math.abs(leftSum - rightSum));
+        }
+        return minDif;
+    }
+
+
     public static void main(String[] args) {
+        //Test for TapeEquilibrium.
+        int[] list = {3, 1, 2, 4, 3};
+        int [] test = {-1000, 1000};
+        int tape = tapeEquilibrium(list);
+        int test1 = tapeEquilibrium(test);
+        System.out.println(test1);
 
     }
 }
--- Java/codility/src/lesson4.java
+++ Java/codility/src/lesson4.java
@@ -1,34 +1,17 @@
-import java.util.Arrays;
-
 /**
  * Created by Martin on 15/11/2015.
  */
 public class lesson4 {
-
-    public static int maxProductofThree(int[] A) {
-        Arrays.sort(A);
-
-        int maxTriplet = 0;
-        int N = A.length;
-
-        //Check if first & second is negative. third is positive
-        if (A[0] * A[1] > 0 && A[0] < 0) {
-            if (A[0] * A[1] * A[N - 1] > A[N - 3] * A[N - 2] * A[N - 1]) {
-                maxTriplet = A[0] * A[1] * A[N - 1];
-            } else {
-                maxTriplet = A[N - 3] * A[N - 2] * A[N - 1];
-            }
-        } else {
-            maxTriplet = A[N - 3] * A[N - 2] * A[N - 1];
-        }
-
-        return maxTriplet;
+    /**
+     * Lesson 4 Counting Elements
+     *
+     */
+    public static int frogRiverOne(int X, int[] A) {
+        // write your code in Java SE 8
+        return 0;
     }
 
     public static void main(String[] args) {
-        //Max product
-        int[] temp = {-3, 1, 2, -2, 5, 6};
-        int res = maxProductofThree(temp);
-        System.out.println(res);
+
     }
 }
