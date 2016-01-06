96cb0eb23011417d5d1a4d9520c111834746738e
martin <dingxin.yu@uqconnect.edu.au>
2016-01-06 20:43:34 +0000

    updated l1 and l2

--- Java/codility/src/lesson1.java
+++ Java/codility/src/lesson1.java
@@ -2,5 +2,20 @@
  * Created by Martin on 15/11/2015.
  */
 public class lesson1 {
+    /**
+     * Lesson1 Iteration
+     * Binary Gap
+     * For example, given N = 1041 the function should return 5, because N has binary representation 10000010001
+     * and so its longest binary gap is of length 5.
+     * Assume that:
+     * N is an integer within the range [1..2,147,483,647].
+     */
+    public int binaryGap(int N){
+        int res = 0;
+        return res;
+    }
 
+    public static void main(String[] args) {
+
+    }
 }
--- Java/codility/src/lesson2.java
+++ Java/codility/src/lesson2.java
@@ -2,6 +2,25 @@
  * Created by Martin on 15/11/2015.
  */
 public class lesson2 {
+    /**
+     * Lesson 2 arrays
+     * OddOccurrencesInArray
+     *
+     * For example, given array A such that:
+     * A[0] = 9  A[1] = 3  A[2] = 9
+     * A[3] = 3  A[4] = 9  A[5] = 7
+     * A[6] = 9
+     * the function should return 7, as explained in the example above.
+     * the element at index 5 has value 7 and is unpaired.
+     *
+     */
+    public int OddOccurrencesInArray(int[] A){
+        int res = 0;
 
+        return res;
+    }
 
+    public static void main(String[] args) {
+
+    }
 }
