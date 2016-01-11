2645f6bde031cd4df184d8747f198f652f0d7030
martin <dingxin.yu@uqconnect.edu.au>
2016-01-11 23:48:32 +0000

    lesson1 binary gap

--- Java/codility/src/lesson1.java
+++ Java/codility/src/lesson1.java
@@ -10,12 +10,29 @@ public class lesson1 {
      * Assume that:
      * N is an integer within the range [1..2,147,483,647].
      */
-    public int binaryGap(int N){
+    public static int binaryGap(int N) {
         int res = 0;
+        int count = -1;
+        int r;
+
+        while (N > 0) {
+            // get right most bit & shift right
+            r = N & 1;
+            N = N >> 1;
+
+            if (0 == r && count >= 0) {
+                count++;
+            }
+            if (1 == r) {
+                res = count > res ? count : res;
+                count = 0;
+            }
+        }
+
         return res;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(binaryGap(1041));
     }
 }
