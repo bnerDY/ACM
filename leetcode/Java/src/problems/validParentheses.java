9abfeca7ef5b25fbda9f27277c3fbe0a81681ec1
martin <dingxin.yu@uqconnect.edu.au>
2016-02-29 09:49:01 +0000

    update

--- leetcode/Java/src/problems/validParentheses.java
+++ leetcode/Java/src/problems/validParentheses.java
@@ -1,6 +1,6 @@
 package problems;
 
-import java.util.Stack;
+import java.util.LinkedList;
 
 /**
  * Created by Martin on 26/02/2016.
@@ -8,25 +8,51 @@ import java.util.Stack;
 public class validParentheses {
 
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<Character>();
-        for (Character c : s.toCharArray()) {
-            if ("({[".contains(String.valueOf(c))) {
+        LinkedList<Character> stack = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '{' || c == '[' || c == '(') {
                 stack.push(c);
             } else {
-                if (!stack.isEmpty() && is_valid(stack.peek(), c)) {
-                    stack.pop();
-                } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char corresponding = stack.pop();
+                if (c == ')' && corresponding != '(') {
+                    return false;
+                }
+                if (c == ']' && corresponding != '[') {
+                    return false;
+                }
+                if (c == '}' && corresponding != '{') {
                     return false;
                 }
             }
         }
-        return stack.isEmpty();
+        return stack.isEmpty() ? true : false;
     }
 
-    private boolean is_valid(char c1, char c2) {
-        return (c1 == '(' && c2 == ')') || (c1 == '{' && c2 == '}')
-                || (c1 == '[' && c2 == ']');
-    }
+//    Alternative solution.
+//    public boolean isValid(String s) {
+//        Stack<Character> stack = new Stack<Character>();
+//        for (Character c : s.toCharArray()) {
+//            if ("({[".contains(String.valueOf(c))) {
+//                stack.push(c);
+//            } else {
+//                if (!stack.isEmpty() && is_valid(stack.peek(), c)) {
+//                    stack.pop();
+//                } else {
+//                    return false;
+//                }
+//            }
+//        }
+//        return stack.isEmpty();
+//    }
+//
+//    private boolean is_valid(char c1, char c2) {
+//        return (c1 == '(' && c2 == ')') || (c1 == '{' && c2 == '}')
+//                || (c1 == '[' && c2 == ']');
+//    }
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
