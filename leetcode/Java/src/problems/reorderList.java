dbfd270f66836a9125b62ad2726f26b6d7df64ab
martin <dingxin.yu@uqconnect.edu.au>
2016-03-07 19:39:18 +0000

    no message

--- leetcode/Java/src/problems/reorderList.java
+++ leetcode/Java/src/problems/reorderList.java
@@ -7,12 +7,21 @@ import problems.ADT.ListNode;
  */
 public class reorderList {
 
-    public void reorderList(ListNode head) {
+    public static void reorderList(ListNode head) {
 
     }
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
+        ListNode test = new ListNode(1);
+        test.next = new ListNode(2);
+        test.next.next = new ListNode(3);
+        test.next.next.next = new ListNode(4);
 
+        reorderList(test);
+        while (test != null) {
+            System.out.println(test.val);
+            test = test.next;
+        }
     }
 }
