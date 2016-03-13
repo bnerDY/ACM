660d3070418cea0820ac2be3a38724c041272b47
martin <dingxin.yu@uqconnect.edu.au>
2016-03-13 21:57:51 +0000

    no message

---
+++ leetcode/Java/src/problems/deleteNodeInALinkedList.java
@@ -0,0 +1,28 @@
+package problems;
+
+import problems.ADT.ListNode;
+
+/**
+ * Created by Martin on 13/03/2016.
+ */
+public class deleteNodeInALinkedList {
+
+    /**
+     Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+     Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should
+     become 1 -> 2 -> 4 after calling your function.
+     * @param node
+     */
+
+    public static void deleteNode(ListNode node) {
+        
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        ListNode test = new ListNode(1);
+        test.next = new ListNode(2);
+        test.next.next = new ListNode(3);
+        test.next.next.next = new ListNode(4);
+    }
+}
