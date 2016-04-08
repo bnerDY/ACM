5a56123a1c7fca66078969d44b033501633c9196
martin <dingxin.yu@uqconnect.edu.au>
2016-04-08 11:53:17 +0100

    no message

---
+++ leetcode/Java/src/problems/nimGame.java
@@ -0,0 +1,16 @@
+package problems;
+
+/**
+ * Created by Martin on 08/04/2016.
+ */
+public class nimGame {
+
+    public boolean canWinNim(int n) {
+        return (n % 4 != 0);
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+}
