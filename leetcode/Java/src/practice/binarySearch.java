a9bf7e03bd0d2589d68bbeb626b1155610a8d429
Dingxin Yu <dingxin.yu@uqconnect.edu.au>
2016-09-18 15:58:25 +0800

    Update binarySearch.java

--- leetcode/Java/src/practice/binarySearch.java
+++ leetcode/Java/src/practice/binarySearch.java
@@ -4,37 +4,35 @@ package practice;
  * Created by Martin on 17/09/2016.
  */
 public class binarySearch {
-    public int binarySearch(int a[], int n, int key) {
-        int low = 0, high = n - 1;
-        int mid;
-        while (low <= high) {
-            mid = low + (high - low) / 2;
-            if (key < a[mid])
-                high = mid - 1;
-            else if (key > a[mid])
-                low = mid + 1;
-            else
-                return mid;
+    public int binarySearch(int a[], int key) {
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo <= hi) {
+            // Key is in a[lo..hi] or not present.
+            int mid = lo + (hi - lo) / 2;
+            if      (key < a[mid]) hi = mid - 1;
+            else if (key > a[mid]) lo = mid + 1;
+            else return mid;
         }
-        return -(low + 1);
+        return -1;
     }
 
-    public int binarySearchRecursive(int a[],int low,int high,int key){
-        if(low>high)
-            return -(low+1);
+    public int binarySearchRecursive(int a[],int low, int high, int key){
+        if(low > high)
+            return -1;
 
-        int mid=low+(high-low)/2;
-        if(key<a[mid])
-            return binarySearchRecursive(a,low,mid-1,key);
+        int mid = low + (high - low)/2;
+        if(key < a[mid])
+            return binarySearchRecursive(a, low, mid - 1, key);
         else if(key > a[mid])
-            return binarySearchRecursive(a,mid+1,high,key);
+            return binarySearchRecursive(a, mid + 1, high, key);
         else
             return mid;
 
     }
 
-    public int binarySearchRecursive(int a[],int n,int key){
-        return binarySearchRecursive(a,0,n-1,key);
+    public int binarySearchRecursive(int a[], int key){
+        return binarySearchRecursive(a, 0, a.length-1, key);
     }
 
 }
