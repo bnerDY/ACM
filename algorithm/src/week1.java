467af22b881742eb2969a5f36553f67ad241e216
martin <dingxin.yu@uqconnect.edu.au>
2016-10-27 22:05:41 +0800

    week1

---
+++ algorithm/src/week1.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Martin on 27/10/2016.
+ */
+public class week1 {
+/**
+ * So: what's the product of the following two 64-digit numbers?
+ * 3141592653589793238462643383279502884197169399375105820974944592
+ * 2718281828459045235360287471352662497757247093699959574966967627
+ */
+
+
+
+public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+}
+
+}
